cafc92d1126005c8409cc1b854e1ac19bc470430
David Anthoff <anthoff@berkeley.edu>
2019-11-09 20:14:28 -0800

    Updates

--- src/PkgButler.jl
+++ src/PkgButler.jl
@@ -1,5 +1,14 @@
 module PkgButler
 
-greet() = print("Hello World!")
+function update_pkg(path::AbstractString)
+    path_for_butler_workflows_folder = joinpath(path, ".github", "workflows", "jlbutler")
+    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "butler-workflow.yml")
+
+    mkpath(path_for_butler_workflows_folder)
+
+    cp(joinpath(@__DIR__, "..", "templates", "butler-workflow.yml"), path_for_main_butler_workflow, force=true)
+
+    cp(joinpath(@__DIR__, "..", "sillytestfile.md"), joinpath(path, "sillytestfile.md"))
+end
 
 end # module
