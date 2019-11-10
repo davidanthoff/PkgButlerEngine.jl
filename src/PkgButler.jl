727e2e22c82a99eed0816359adcfaefaf1a8ea4f
David Anthoff <anthoff@berkeley.edu>
2019-11-10 15:59:57 -0800

    Update templates

--- src/PkgButler.jl
+++ src/PkgButler.jl
@@ -1,12 +1,14 @@
 module PkgButler
 
 function update_pkg(path::AbstractString)
-    path_for_butler_workflows_folder = joinpath(path, ".github", "workflows", "jlbutler")
-    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "butler-workflow.yml")
+    path_for_butler_workflows_folder = joinpath(path, ".github", "workflows")
+    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-workflow.yml")
+    path_for_ci_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-ci-workflow.yml")
 
     mkpath(path_for_butler_workflows_folder)
 
-    cp(joinpath(@__DIR__, "..", "templates", "butler-workflow.yml"), path_for_main_butler_workflow, force=true)
+    cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-butler-workflow.yml"), path_for_main_butler_workflow, force=true)
+    cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-ci-workflow.yml"), path_for_ci_workflow, force=true)
 end
 
 end # module
