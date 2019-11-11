48a1950051f0732a5d03d543fb22ea15f6d27082
David Anthoff <anthoff@berkeley.edu>
2019-11-10 17:45:06 -0800

    Update templates

--- src/PkgButlerEngine.jl
+++ src/PkgButlerEngine.jl
@@ -2,13 +2,21 @@ module PkgButlerEngine
 
 function update_pkg(path::AbstractString)
     path_for_butler_workflows_folder = joinpath(path, ".github", "workflows")
-    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-workflow.yml")
-    path_for_ci_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-ci-workflow.yml")
+
+    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-workflow.yml")    
+    path_for_ci_master_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-ci-master-workflow.yml")
+    path_for_ci_pr_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-ci-pr-workflow.yml")
 
     mkpath(path_for_butler_workflows_folder)
 
     cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-butler-workflow.yml"), path_for_main_butler_workflow, force=true)
-    cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-ci-workflow.yml"), path_for_ci_workflow, force=true)
+    cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-ci-master-workflow.yml"), path_for_ci_master_workflow, force=true)
+    cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-ci-pr-workflow.yml"), path_for_ci_pr_workflow, force=true)
+
+    path_for_old_ci_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-ci-workflow.yml")
+    if isfile(path_for_old_ci_workflow)
+        rm(path_for_old_ci_workflow)
+    end
 end
 
 end # module
