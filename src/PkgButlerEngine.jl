666413ac6878bfdaeb31813d6d7bcd9d55c0a0ba
David Anthoff <anthoff@berkeley.edu>
2019-11-16 16:59:09 -0800

    Add configure_pkg function

--- src/PkgButlerEngine.jl
+++ src/PkgButlerEngine.jl
@@ -1,8 +1,35 @@
 module PkgButlerEngine
 
-function update_pkg(path::AbstractString; channel=:auto)
+function configure_pkg(path::AbstractString; channel=:auto)
     channel in (:auto, :stable, :dev) || error("Invalid value for channel.")
 
+    path_for_butler_workflows_folder = joinpath(path, ".github", "workflows")
+
+    path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-workflow.yml")
+    path_for_main_butler_dev_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-dev-workflow.yml")
+
+    mkpath(path_for_butler_workflows_folder)
+
+    if channel==:auto
+        channel = isfile(path_for_main_butler_dev_workflow) ? :dev : :stable
+    end
+
+    if channel==:stable
+        if isfile(path_for_main_butler_dev_workflow)
+            rm(path_for_main_butler_dev_workflow, force=true)
+        end
+
+        cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-butler-workflow.yml"), path_for_main_butler_workflow, force=true)
+    elseif channel==:dev
+        if isfile(path_for_main_butler_workflow)
+            rm(path_for_main_butler_workflow, force=true)
+        end
+
+        cp(joinpath(@__DIR__, "..", "templates", "jlpkgbutler-butler-dev-workflow.yml"), path_for_main_butler_dev_workflow, force=true)
+    end
+end
+
+function update_pkg(path::AbstractString)
     path_for_butler_workflows_folder = joinpath(path, ".github", "workflows")
 
     path_for_main_butler_workflow = joinpath(path_for_butler_workflows_folder, "jlpkgbutler-butler-workflow.yml")
@@ -15,9 +42,7 @@ function update_pkg(path::AbstractString; channel=:auto)
 
     mkpath(path_for_butler_workflows_folder)
 
-    if channel==:auto
-        channel = isfile(path_for_main_butler_dev_workflow) ? :dev : :stable
-    end
+    channel = isfile(path_for_main_butler_dev_workflow) ? :dev : :stable
 
     if channel==:stable
         if isfile(path_for_main_butler_dev_workflow)
