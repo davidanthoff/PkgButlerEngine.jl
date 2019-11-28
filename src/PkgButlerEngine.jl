9a898531e28d80dc7eb6003faeaca37b6d7be58e
David Anthoff <anthoff@berkeley.edu>
2019-11-27 17:06:05 -0800

    For now use full julia version numbers

--- src/PkgButlerEngine.jl
+++ src/PkgButlerEngine.jl
@@ -125,7 +125,7 @@ function construct_version_matrix(path)
 
     version_spec = Pkg.Types.semver_spec(julia_compat_bound)
 
-    versions = [v"1.0.5"=>"1.0", v"1.1.1"=>"1.1", v"1.2.0"=>"1.2", v"1.3.0"=>"1.3"]
+    versions = [v"1.0.5"=>"1.0.5", v"1.1.1"=>"1.1.1", v"1.2.0"=>"1.2.0", v"1.3.0"=>"1.3.0"]
 
     compat_versions = filter(i->i[1] in version_spec, versions)
 
